f2f3e8635e88ad38af8b4c8cfd3b25623e876b5d
pvarikuntla <pvarikuntla@DXCINDAI694692.asiapac.globalcsc.net>
2019-01-11 04:52:00 -0500

    added new method in existing class

--- Sample/src/com/pppp.java
+++ Sample/src/com/pppp.java
@@ -3,6 +3,11 @@ package com;
 public class pppp {
 	public static void main(String[] args) {
 		System.out.println("this is main method");
+		addition(4,9);
+	}
+	public static void addition(int a,int b) 
+	{
+		System.out.println(a+b);
 	}
 
 }
